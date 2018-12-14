534fa48e9239d2ed967ad7acde292ced8c119918
Beatriz Tovar <beatrizctovar@gmail.com>
2018-12-14 14:55:42 -0800

    adding content and button to modal

--- mailing-list-modal/src/components/MailingListModal.js
+++ mailing-list-modal/src/components/MailingListModal.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Modal, Button } from 'react-bootstrap'
 
 class MailingListModal extends React.Component {
     constructor(props){
@@ -12,7 +13,24 @@ class MailingListModal extends React.Component {
     render() {
         return(
             <React.Fragment>
-                something!
+                <Modal className='modal modal-dialog' show={this.state.open} onHide={this.onClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>
+                    <span>Would you like to join our mailing list?</span>
+                </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div>
+                        By joining, you would have access to the latest news!
+                    </div>
+                    <div>
+                    <input className='form-control' type='email' name='email' value={this.state.email} placeholder='Enter Email...' onChange={this.valueChange} />
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button className='submit-btn' onClick={this.onClose}>Yes, Count Me In!</Button>
+                </Modal.Footer>
+                </Modal>
             </React.Fragment>
         )
     }
