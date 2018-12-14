1d3d9d7c45d814f9572326ee3d682a2ebe303408
Beatriz Tovar <beatrizctovar@gmail.com>
2018-12-14 14:48:20 -0800

    added modules

---
+++ mailing-list-modal/src/components/MailingListModal.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class MailingListModal extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+            open: false,
+            email: ''
+        }
+    }
+
+    render() {
+        return(
+            <React.Fragment>
+                something!
+            </React.Fragment>
+        )
+    }
+}
+
+export default MailingListModal;
