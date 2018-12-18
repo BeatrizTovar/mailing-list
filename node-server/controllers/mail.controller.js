402c2757dcd02ede2f6d3842883eea9cba4a2d8a
Beatriz Tovar <beatrizctovar@gmail.com>
2018-12-17 18:02:53 -0800

    wip

--- node-server/controllers/mail.controller.js
+++ node-server/controllers/mail.controller.js
@@ -1,7 +1,22 @@
-const read = (req, res) => {
-    res.send("Hellow World!")
-}
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
+// const read = (req, res) => {
+//     res.send("Hellow World!")
+// } test
+
+const sendList = (req, res) => {
+    const msg = {
+        to: 'beatrizctovar@gmail.com',
+        from: 'test@example.com',
+        subject: 'Sending with SendGrid is Fun?',
+        test: 'and easy to do anywhere, even with Node.js?',
+        html: '<strong> and easy to do anywhere, even with Node.js></strong>',
+    };
+
+    sgMail.send(msg);
+}
 module.exports = {
-    read
+    // read
+    sendList
 }
