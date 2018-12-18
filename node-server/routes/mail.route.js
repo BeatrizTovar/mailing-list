bae9a0df21d1190e9c884e479f6e577c10df662e
Beatriz Tovar <beatrizctovar@gmail.com>
2018-12-17 17:36:07 -0800

    installed sendgrid

--- node-server/routes/mail.route.js
+++ node-server/routes/mail.route.js
@@ -1,8 +1,7 @@
 const router = require('express').Router()
 const mailController = require('../controllers/mail.controller')
 
-router.get('/', function (req, res) {
-    res.send("asdfdsf")
-})
+// router.get('/', mailController.read) tester route
+router.post('/send-updates', mailController.sendList)
 
 module.exports = router;
