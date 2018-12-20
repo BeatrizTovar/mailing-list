70e24a63eff8a55a1dea467d1c9fa6c460dff4fd
Beatriz Tovar <beatrizctovar@gmail.com>
2018-12-19 21:32:20 -0800

    modified automated email message

--- node-server/controllers/mail.controller.js
+++ node-server/controllers/mail.controller.js
@@ -1,22 +1,20 @@
 const sgMail = require('@sendgrid/mail')
-// sgMail.setApiKey()
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-const sendList = (req, res) => {    const { query: { to = 'dammitSoo@dispostable.com', from = 'goddammitSoo@dispostable.com' } } = req;
+const sendList = (req, res) => {    const { query: { from = 'goddammitSoo@dispostable.com' } } = req;
     // other options could be customized further
     
     const msg = {
-        to,
+        to: req.body.to,
         from,
-        subject: 'Is this going to work?',
-        text: 'and easy to do anywhere, even with Node.js',
-        html: '<strong>Hello Email app</strong>',
+        subject: 'Thank you!',
+        text: 'Now that you have signed up, you will get access to the lastest news!',
+        html: '<strong>Thank You!</strong>',
         templateId: '4c90a9b7-f7d3-4516-ab4c-efc6f959ed99'
     };
 
-
     sgMail.send(msg).then(() => {
-        res.status(200).send('Hello, world!').end();
+        res.status(200).end();
     }).catch(e => {
         console.error(e.toString());
         res.status(500).end();
